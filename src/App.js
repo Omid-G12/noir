6801bf4a98ecf8217b68057404dc35495a5de0cb
Omid_G12 <omid.12.ghari@gmail.com>
2022-12-01 15:52:43 -0800

    sidebar and navbar base added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import './App.css';
 
 const App = () => {
+  const activeMenu = true;
   return (
-    <h1 className='underline text-3xl'>App</h1>
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent content="Settings" position='Top'>
+              <button type='button' 
+              className='text-3xl p-3
+              hover:drop-shadow-xl
+              hover:bg-light-gray text-white'
+              style={{ background: 'blue', 
+              borderRadius: '50%' }}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className='w-72 fized sidebar
+            dark:bg-secondary-dark-bg
+            bg-white'>
+              Sidebar
+            </div>
+          ) : (
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              Sidebar w-0
+            </div>
+          )}
+          <div className={
+            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:m1-72' : 'flex-2'}`
+            }>
+            <div className='fixed md:static
+            bg-main-bg dark:bg-main-dark-bg
+            navbar w-full'>
+
+            </div>
+          </div>
+
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path='/' element='ECommerce' />
+              <Route path='/ecommerce' element='ECommerce' />
+
+              {/* Pages */}
+              <Route path='/orders' element='Pages' />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
